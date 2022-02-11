304c02b2a19d4c80c847ae616d952fac3ae0d5c7
wjchulme <25637345+wjchulme@users.noreply.github.com>
2022-02-11 11:22:52 +0000

    use local var labels (not in design script)

--- analysis/table1.R
+++ analysis/table1.R
@@ -39,8 +39,46 @@ data_cohort <- read_rds(here("output", "data", "data_cohort.rds")) %>%
     )
   )
 
-var_labels <-splice(N = N  ~ "Total N", var_labels)
-var_labels <-splice(allpop = allpop  ~ "All", var_labels)
+
+
+## variable labels
+var_labels <-
+  list(
+    N  ~ "Total N",
+    allpop  ~ "All",
+    vax12_type ~ "Primary vaccination course (doses 1 and 2)",
+    age ~ "Age",
+    ageband ~ "Age",
+    sex ~ "Sex",
+    ethnicity_combined ~ "Ethnicity",
+    imd_Q5 ~ "IMD",
+    region ~ "Region",
+    rural_urban_group ~ "Rural/urban category",
+    stp ~ "STP",
+    jcvi_group ~ "JCVI group",
+    cev ~ "Clinically extremely vulnerable",
+
+    sev_obesity ~ "Body Mass Index > 40 kg/m^2",
+
+    chronic_heart_disease ~ "Chronic heart disease",
+    chronic_kidney_disease ~ "Chronic kidney disease",
+    diabetes ~ "Diabetes",
+    chronic_liver_disease ~ "Chronic liver disease",
+    chronic_resp_disease ~ "Chronic respiratory disease",
+    chronic_neuro_disease ~ "Chronic neurological disease",
+    immunosuppressed ~ "Immunosuppressed",
+    asplenia ~ "Asplenia or poor spleen function",
+
+    learndis ~ "Learning disabilities",
+    sev_mental ~ "Serious mental illness",
+
+    multimorb ~ "Morbidity count",
+
+    prior_covid_infection0 ~ "Prior SARS-CoV-2 infection",
+    prior_tests_cat ~ "Number of SARS-CoV-2 tests in previous 3 months"
+
+  ) %>%
+  set_names(., map_chr(., all.vars))
 
 ## baseline variables
 tab_summary_baseline <- data_cohort %>%
