1c73d4c484ea148e2a59e124f8dec267a909bda6
wjchulme <25637345+wjchulme@users.noreply.github.com>
2022-03-01 15:00:40 +0000

    Update combine_model.R

--- analysis/combine_model.R
+++ analysis/combine_model.R
@@ -349,6 +349,7 @@ incidence <-
 
 write_csv(incidence, fs::path(output_dir, "incidence.csv"))
 
+incidence %>% filter(subgroup=="none", period=="all") %>% write_csv(fs::path(output_dir, "incidence_all_none.csv"))
 
 gt_incidence <-
   incidence %>%
